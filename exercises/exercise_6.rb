0ddcaeb311f33e69b007f58bf579a79909e687d3
Meg Wilken <102841984+Megwilken@users.noreply.github.com>
2023-05-05 11:49:41 -0600

    add employees to database

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -9,3 +9,19 @@ puts "Exercise 6"
 puts "----------"
 
 # Your code goes here ...
+class Store
+  has_many :employees
+end
+
+class Employee 
+  belongs_to :store
+end
+
+@store1 = Store.find(1)
+@store2 = Store.find(2)
+
+@store1.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)
+@store1.employees.create(first_name: "Alice", last_name: "Jones", hourly_rate: 96)
+@store2.employees.create(first_name: "Bob", last_name: "Smith", hourly_rate: 60)
+@store2.employees.create(first_name: "Carol", last_name: "Lee", hourly_rate: 50)
+@store2.employees.create(first_name: "Dave", last_name:" Wang", hourly_rate: 80)

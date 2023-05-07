14af7a7ca714f689ebcac21bdf7280fb6496cdcd
Meg Wilken <102841984+Megwilken@users.noreply.github.com>
2023-05-07 14:03:39 -0600

    add validations to models

--- exercises/exercise_7.rb
+++ exercises/exercise_7.rb
@@ -10,3 +10,26 @@ puts "Exercise 7"
 puts "----------"
 
 # Your code goes here ...
+class Store 
+  has_many :employees
+  
+  validates :name, presence: true, length: { minimum: 3 }
+  validates :annual_revenue, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  validate :must_carry_apparel
+
+
+  def must_carry_apparel
+    if !mens_apparel && !womens_apparel
+      errors.ass(:base, "must carry at least one of men's or women's apparel")
+    end
+  end
+end
+
+class Employee 
+  belongs_to :store
+
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :hourly_rate, numericality: { only_integer: true, greater_than_or_equal_to: 40, less_than_or_equal_to: 200 }
+  validates_associated :store
+end
